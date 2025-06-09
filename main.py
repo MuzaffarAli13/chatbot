9d6a19c4c12eafeb18bbee07720637626ee8b783
Muzaffar Ali <muzafark834@gmail.com>
2025-06-09 12:00:38 +0500

    add port

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-import chainlit as cl
-from chatbot import myAgent
-import asyncio
- 
-@cl.on_chat_start
-async def on_chat_start():
-    await cl.Message("Hello How I can Help you?").send()
-
-@cl.on_message
-async def on_message(message: cl.Message):
-    user_input = message.content
-    response = asyncio.run(myAgent(user_input))
-    await cl.Message(
-        content=f"{response}",
-    ).send()
 
 import chainlit as cl
 from chatbot import myAgent
-import asyncio
+import asyncio 
 import os
 
 # ✅ Optional: Manually set PORT for Railway (Chainlit uses this if needed)
@@ -30,4 +15,4 @@ async def chat_start():
 async def main(message: cl.Message):
     user_input = message.content
     response = await myAgent(user_input)  # ✅ asyncio.run hata diya
-    await cl.Message(content=f"{response}").send()
+    await cl.Message(content=f"{response}").send() 
